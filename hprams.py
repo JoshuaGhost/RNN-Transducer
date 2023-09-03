e88ac27dbfaac5af630aec0ab7d810ea9ea59289
Guang Han <sniperstriker@gmail.com>
2023-09-03 15:04:07 +0200

    clear hydra instance if there is one already

--- hprams.py
+++ hprams.py
@@ -1,5 +1,9 @@
 from hydra import compose, initialize
 from sys import argv
+import hydra
 
+if hydra.core.global_hydra.GlobalHydra.instance() is not None:
+    hydra.core.global_hydra.GlobalHydra.instance().clear()
 initialize(config_path="config")
-hprams = compose(config_name="configs", overrides=argv[1:])
+# hprams = compose(config_name="configs", overrides=argv[1:])
+hprams = compose(config_name="configs")
